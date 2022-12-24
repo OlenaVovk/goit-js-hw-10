524aa299a030e0d7c9f03579d75963b9ace2041f
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-12-24 20:51:30 +0200

    fix render

--- src/index.js
+++ src/index.js
@@ -9,27 +9,28 @@ const divEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-
 function onInput () {
  console.log(inputEl.value);
  return fetchCountries(inputEl.value).then(data => {
     
     if (data.length > 10) {
-       console.log("so sorry");
-    
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        listEl.innerHTML = '';
+        divEl.innerHTML = '';
+        return;
     }
     if (data.length > 1 && data.length < 10) {
-       renderCountries(data);
+        renderCountries(data);
     }
     if (data.length === 1) {
         renderCountry(...data);
     }
+    if (data.length === 0) {
+        
+    }
+     
   })
-
-
 }
-
-
 
 function fetchCountries(name) {
    return fetch(`https://restcountries.com/v3.1/name/${name}`)
@@ -39,18 +40,22 @@ function fetchCountries(name) {
         }
     return response.json();
     })
-    .catch(error => console.log("УУупс!", error))
+    .catch(error => {
+        console.log(error);
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+    });
 }
 
 function renderCountry({name, capital, population, flags, languages}) {
-
+    listEl.innerHTML = '';
     divEl.innerHTML = `<img src="${flags.svg}" alt="flat" width="20px"/>
                     <h2>${name.official}</h2>
                     <p>Capital: ${capital}</p>
                     <p>Population: ${population}</p>
-                    <p>Languages: ${languages}</p>`;
+                    <p>Languages: ${Object.values(languages)}</p>`;
 }
 
 function renderCountries(data) {
-    listEl.innerHTML = data.map(item => (`<li><img src="${item.flags.svg}" alt="flat" width="20px"/><span>${item.name.official}</span></li>`));  
+    listEl.innerHTML = data.map(item => (`<li><img src="${item.flags.svg}" alt="flat" width="20px"/><span>${item.name.official}</span></li>`)).join(''); 
+    divEl.innerHTML = '';
 }
